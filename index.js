8cd9b0ce8bd5a22385d67b8dace94c5af9333c54
alfazachmmosi@TimeMachine <zalfa3890@gmail.com>
2025-03-12 10:41:11 +0300

    modified file : index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+  
+}
+saturdayFun('This Saturday, I want to bathe my dog!');
+
+
+(function(baseNumber) {
+    return baseNumber + 3;
+})(2);
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+console.log(mondayWork());
+console.log(mondayWork('work from home.'));
+
+function wrapAdjective(wrapper = "*") {
+    return function(adjective = "special") {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+  }
